f0d36a2472fd0642b5b550182a2bc42532151036
asahinashuji <box6409@gmail.com>
2016-02-02 11:24:35 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   #名前は必須入力かつ２０文字以内
   validates :name , length: {maximum: 20 } , presence: true
+  #年齢は０以上
+  validates :age , numericality: {greater_than_or_equal_to: 0} ,presence: true
   #内容は必須入力かつ２文字以上３０文字以下
   validates :body , length: {minimum: 2, maximum: 30} , presence: true
 end
